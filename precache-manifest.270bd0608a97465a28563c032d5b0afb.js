248b9a229e660dfe761a2868da11736c72d87593
Neetu Chauhan <neetuchauhan0421@gmail.com>
2020-09-30 17:16:51 +0530

    Updates

---
+++ precache-manifest.270bd0608a97465a28563c032d5b0afb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4590ba2154bc6857e99fdecc9e1716",
+    "url": "/ScalenutDemo/index.html"
+  },
+  {
+    "revision": "b23b2d144a0280f36124",
+    "url": "/ScalenutDemo/static/css/2.b9c16c38.chunk.css"
+  },
+  {
+    "revision": "f9567fe7bd166958c5c5",
+    "url": "/ScalenutDemo/static/css/main.5f79e145.chunk.css"
+  },
+  {
+    "revision": "b23b2d144a0280f36124",
+    "url": "/ScalenutDemo/static/js/2.6e0da662.chunk.js"
+  },
+  {
+    "revision": "14d3db2f0aae1bbe6b1122194ce299b3",
+    "url": "/ScalenutDemo/static/js/2.6e0da662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9567fe7bd166958c5c5",
+    "url": "/ScalenutDemo/static/js/main.77e1e740.chunk.js"
+  },
+  {
+    "revision": "40a1d3eac017fcc8900c",
+    "url": "/ScalenutDemo/static/js/runtime-main.75a35ec4.js"
+  },
+  {
+    "revision": "bfef62c8152f5c3d0a859f6c72aa8841",
+    "url": "/ScalenutDemo/static/media/google.bfef62c8.png"
+  },
+  {
+    "revision": "e7bed808096eee204141d2c304fff9a8",
+    "url": "/ScalenutDemo/static/media/group-215.e7bed808.png"
+  }
+]);
